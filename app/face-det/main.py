c9a427b747b4ce8e65132ea34da6fb14312b68c1
CaptainJackey <237827161@qq.com>
2026-07-13 15:25:10 +0800

    Optimize code

--- app/face-det/main.py
+++ app/face-det/main.py
@@ -1,23 +1,29 @@
-import cv2
-import time
+'''
+实验名称：人脸检测
+实验平台：CyberCAM
+说明：单个或多个人脸检测
+'''
 
-from walnutpi import Display,Sensor,kpu,IDE
-#【可选代码】允许Thonny远程运行
-import os
-os.environ["DISPLAY"] = ":0.0"
+import cv2, time, os
+from walnutpi import Display,Sensor,kpu,IDE,direction
+
+# 优先当前文件夹下相对路径（app离线部署）
+if os.path.exists("./face_detection_320.kmodel") and os.path.exists("./prior_data_320.bin"):
+    model_path = "./face_detection_320.kmodel"
+    anchors_path = "./prior_data_320.bin"
+
+# 使用系统绝对路径（IDE运行调试）
+elif os.path.exists("/data/app/face-det/face_detection_320.kmodel") and os.path.exists("/data/app/face-det/prior_data_320.bin"):
+    model_path = "/data/app/face-det/face_detection_320.kmodel"
+    anchors_path = "/data/app/face-det/prior_data_320.bin"
+else:
+    raise FileNotFoundError("模型文件缺失，请检查当前路径与系统路径下的模型文件是否存在。")
+
+model_size = 320 #模型尺寸
+detector = kpu.FACE_DETECT(model_path, anchors_path, model_size) #加载模型
 
 # 初始化屏幕
 Display.init()
-
-# 初始化人脸检测器，使用320x320模型
-kmodel_path = "./face_detection_320.kmodel"
-anchors_path = "./prior_data_320.bin"
-detector = kpu.FACE_DETECT(kmodel_path, anchors_path,320)
-
-# 初始化人脸检测器，使用640x640模型
-# kmodel_path = "./face_detection_640.kmodel"
-# anchors_path = "./prior_data_640.bin"
-# detector = kpu.FACE_DETECT(kmodel_path, anchors_path,640)
 
 # 初始化摄像头
 cap = Sensor.Sensor(640, 480)
@@ -25,33 +31,46 @@ if not cap.isOpened():
     print("Cannot open camera")
     exit()
 
-#计算帧率
-last_time = time.time()
+#获取当前显示屏方向，0表示默认，2表示180度翻转。
+lcd_dir=direction.get_lcd() 
+#print(lcd_dir) 
+
+# 判断显示屏是否翻转，如果翻转，则设置显示旋转180°，摄像头同时设置为前置模式（水平镜像）
+if lcd_dir == 2: #翻转了
+    Display.set_rotation(2)
+    cap.set_hmirror(1)
+
+# ========== FPS计算 ==========
+frame_count = 0       # 帧数计数器
+start_time = time.time()
 fps = 0.0
+
 while True:
-    
-    #计算帧率
-    current_time = time.time()
-    delta = current_time - last_time
-    last_time = current_time
-    if delta > 0:
-        fps = round(1.0 / delta, 1)
-    else:
-        fps = 0.0
-    print("FPS: ", fps)
     
     # 摄像头读取一帧图像    
     ret, img = cap.read()
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
     
     # 执行目标检测，设置置信度阈值为 0.6，IoU 阈值为 0.7
-    result = detector.run(img, reliability_threshold=0.6, nms_threshold=0.7)
-        
-    
+    results = detector.run(img, reliability_threshold=0.6, nms_threshold=0.7)
+
     # 打印并绘制结果
-    for result in result:
+    for result in results:
+
+        # 打印结果，当人脸数量过多时会降低识别帧率。
+        '''
+        # ========== 人脸框坐标 ==========
+        print("=" * 40)
+        print(f"人脸框: 左上角({result.x}, {result.y})  宽:{result.w}  高:{result.h}")
+        print(f"置信度: {result.reliability:.3f}")
+        
+        # ========== 5个关键点坐标 ==========
+        print(f"左眼   : ({result.left_eye.x:3d}, {result.left_eye.y:3d})")
+        print(f"右眼   : ({result.right_eye.x:3d}, {result.right_eye.y:3d})")
+        print(f"鼻子   : ({result.nose.x:3d}, {result.nose.y:3d})")
+        print(f"左嘴角 : ({result.left_mouth.x:3d}, {result.left_mouth.y:3d})")
+        print(f"右嘴角 : ({result.right_mouth.x:3d}, {result.right_mouth.y:3d})")
+        print("=" * 40)
+        '''
         # 绘制 人脸识别框
         cv2.rectangle(img, (result.x, result.y), 
                         (result.x + result.w, result.y + result.h), 
@@ -69,7 +88,17 @@ while True:
         cv2.circle(img, (result.left_mouth.x, result.left_mouth.y), 4, (0, 255, 0), -1) # 左嘴角
         cv2.circle(img, (result.right_mouth.x, result.right_mouth.y), 4, (255, 0, 0), -1) # 右嘴角
 
+    # 每满1秒计算一次平均FPS
+    frame_count += 1    
+    current_time = time.time()
+    if current_time - start_time >= 1.0:
+        fps = frame_count / (current_time - start_time)
+        frame_count = 0              # 重置帧数计数器
+        start_time = current_time    # 重置计时起点
+        print("FPS: ", f'FPS: {fps:.1f}')
 
-    cv2.putText(img, 'FPS: '+str(fps), (10,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2) #图像绘制帧率
+     # 添加文字标签和FPS显示
+    cv2.putText(img, f'FPS: {fps:.1f}', (10, 30), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+    
     Display.show(img) #显示到屏幕上
     IDE.show(img) # 发送到ide窗口内显示
--- app/yolo11-det/main.py
+++ app/yolo11-det/main.py
@@ -1,15 +1,26 @@
 '''
-实验名称：YOLO11检测
-实验平台：cybercam
-说明：摄像头采集检测
+实验名称：物体检测（基于YOLO11）
+实验平台：CyberCAM
+说明：YOLO11检测
 '''
 
-from walnutpi import kpu, Display, Sensor, IDE
-import cv2, time, colorsys
+import cv2, time, os, colorsys
+from walnutpi import kpu, Display, Sensor, IDE, direction
 
+# 优先当前文件夹下相对路径（app离线部署）
+if os.path.exists("./yolo11n.kmodel"):
+    model_path = "./yolo11n.kmodel"
 
-model_size = 224
-model_path = "yolo11n.kmodel"
+# 使用系统绝对路径（IDE运行调试）
+elif os.path.exists("/data/app/yolo11-det/yolo11n.kmodel"):
+    model_path = "/data/app/yolo11-det/yolo11n.kmodel"
+
+else:
+    raise FileNotFoundError("模型文件缺失，请检查当前路径与系统路径下的模型文件是否存在。")
+
+model_size = 224 #模型尺寸
+yolo = kpu.YOLO11_DET(model_path, model_size) # 加载模型 
+
 labels = [
     'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
     'train', 'truck', 'boat', 'traffic light', 'fire hydrant',
@@ -27,44 +38,58 @@ labels = [
     'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
 ]
 
-# 字体配置（文泉驿正黑，字号 20）
-FONT_PATH = "/usr/share/fonts/truetype/wqy/wqy-zenhei.ttc"
-FONT_SIZE = 30
-ft2 = cv2.freetype.createFreeType2()
-ft2.loadFontData(FONT_PATH, 0)
+#字符显示改进，支持中英文显示
+ft = cv2.freetype.createFreeType2() #创建freetype渲染器
+ft.loadFontData("/usr/share/fonts/truetype/wqy/wqy-zenhei.ttc", 0) #加载字体文件, 文泉驿正黑
+
+def putText_Chinese(img, text, org, fontScale=30, color=(0, 255, 0)):
+    
+    global ft  # 使用全局的 FreeType 渲染器实例
+    
+    # 绘制中文
+    ft.putText(
+        img=img,
+        text=text,
+        org=org,
+        fontHeight=fontScale,
+        color=color,
+        thickness=-1,        # 笔画粗细
+        line_type=cv2.LINE_AA,  # 抗锯齿，文字更平滑
+        bottomLeftOrigin=True  # False:坐标为左上角; True:与原生cv2.putText一致（左下角）
+    )
+    return img
 
 def _get_label_color(label_index, num_labels):
     r, g, b = colorsys.hsv_to_rgb(label_index / num_labels, 0.9, 0.8)
     return (int(b * 255), int(g * 255), int(r * 255))
 
-
-# 加载模型 
-yolo = kpu.YOLO11_DET(model_path, model_size)
-
+# 初始化屏幕
 Display.init()
+
+# 初始化摄像头
 cap = Sensor.Sensor(640, 480)
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
 
-last_time = time.time()
-fps = 0.0
-while True:
-    # 读取一帧
-    ret, img = cap.read()
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
+#获取当前显示屏方向，0表示默认，2表示180度翻转。
+lcd_dir=direction.get_lcd() 
+#print(lcd_dir) 
 
-    # ========== 计算帧率：基于帧间时间差 ==========
-    current_time = time.time()
-    delta = current_time - last_time
-    last_time = current_time
+# 判断显示屏是否翻转，如果翻转，则设置显示旋转180°，摄像头同时设置为前置模式（水平镜像）
+if lcd_dir == 2: #翻转了
+    Display.set_rotation(2)
+    cap.set_hmirror(1)
+
+# ========== FPS计算 ==========
+frame_count = 0       # 帧数计数器
+start_time = time.time()
+fps = 0.0
+
+while True:
     
-    if delta > 0:
-        fps = round(1.0 / delta, 1)
-    else:
-        fps = 0.0  # 防止除零（理论上不会发生）
+    # 摄像头读取一帧
+    ret, img = cap.read()
 
     # 阻塞式目标检测
     boxes = yolo.run(img, 0.5, 0.45)
@@ -84,6 +109,9 @@ while True:
 
     # 绘制检测框和中文标签 
     for box in boxes:
+
+        FONT_SIZE = 30 # 字体大小
+
         color = _get_label_color(box.label, len(labels))
         label_text = f"{labels[box.label]} {box.reliability:.2f}"
 
@@ -93,7 +121,7 @@ while True:
         right_y = int(box.y + box.h / 2)
 
         # 获取文字尺寸
-        (label_width, label_height), baseline = ft2.getTextSize(label_text, FONT_SIZE, -1)
+        (label_width, label_height), baseline = ft.getTextSize(label_text, FONT_SIZE, -1)
 
         # 防止标签超出图像顶部
         text_y = left_y - baseline
@@ -103,44 +131,23 @@ while True:
             text_y = label_height
 
         # 画检测框（不同标签不同颜色）
-        cv2.rectangle(
-            img,
-            (left_x, left_y),
-            (right_x, right_y),
-            color,
-            2,
-        )
+        cv2.rectangle(img, (left_x, left_y), (right_x, right_y), color, 2)
 
-        # 绘制标签
-        ft2.putText(
-            img,
-            label_text,
-            (left_x, text_y),
-            FONT_SIZE,
-            color,
-            -1,
-            cv2.LINE_AA,
-            True,
-        )
+        #输出字符
+        putText_Chinese(img, label_text, (left_x, text_y), fontScale=FONT_SIZE, color=color)
 
-    # 绘制 FPS
-    fps_text = f"FPS: {fps}"
-    (fps_w, fps_h), fps_base = ft2.getTextSize(fps_text, FONT_SIZE, -1)
-    ft2.putText(
-        img,
-        fps_text,
-        (10, 10 + fps_h),
-        FONT_SIZE,
-        (0, 0, 255),
-        -1,
-        cv2.LINE_AA,
-        True,
-    )
+    # 每满1秒计算一次平均FPS
+    frame_count += 1    
+    current_time = time.time()
+    if current_time - start_time >= 1.0:
+        fps = frame_count / (current_time - start_time)
+        frame_count = 0              # 重置帧数计数器
+        start_time = current_time    # 重置计时起点
+        print("FPS: ", f'FPS: {fps:.1f}')
+
+    #FPS显示
+    putText_Chinese(img, f'FPS: {fps:.1f}',  (10, 30), fontScale=30, color=(0, 255, 0))
 
     # 显示图像
     Display.show(img)
     IDE.show(img)
-
-# 清理
-cap.release()
-cv2.destroyAllWindows()
